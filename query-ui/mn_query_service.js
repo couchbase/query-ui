9e521664a5188636d2a815957f5decb662089223
Eben Haber <eben@couchbase.com>
2016-03-22 13:18:58 -0700

    MB-18783, change message for running queries to 'statement executing'
    Change-Id: I22da56cd1c7915540178e7b5d05883e919234d8a
    Reviewed-on: http://review.couchbase.org/61839
    Reviewed-by: Eben Haber <eben@couchbase.com>
    Tested-by: Eben Haber <eben@couchbase.com>

--- query-ui/mn_query_service.js
+++ query-ui/mn_query_service.js
@@ -140,8 +140,8 @@
 
     var executingQueryTemplate = dummyResult.clone();
     executingQueryTemplate.status = "executing";
-    executingQueryTemplate.result = '{"status": "Executing Query"}';
-    executingQueryTemplate.data = {status: "Executing Query"};
+    executingQueryTemplate.result = '{"status": "Executing Statement"}';
+    executingQueryTemplate.data = {status: "Executing Statement"};
     executingQueryTemplate.resultSize = 0;
     executingQueryTemplate.resultCount = 0;
 
@@ -378,6 +378,28 @@
       currentQueryIndex = 0;
     }
 
+    /**
+     * Fast UUID generator, RFC4122 version 4 compliant.
+     * @author Jeff Ward (jcward.com).
+     * @license MIT license
+     * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+     **/
+    var UUID = (function() {
+      var self = {};
+      var lut = []; for (var i=0; i<256; i++) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+      self.generate = function() {
+        var d0 = Math.random()*0xffffffff|0;
+        var d1 = Math.random()*0xffffffff|0;
+        var d2 = Math.random()*0xffffffff|0;
+        var d3 = Math.random()*0xffffffff|0;
+        return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+        lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+        lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+        lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+      }
+      return self;
+    })();
+
     //
     // cancelQuery - if a query is running, cancel it
     //
@@ -557,10 +579,10 @@
 
       })
       .error(function(data, status, headers, config) {
-        console.log("Error Data: " + JSON.stringify(data));
-        console.log("Error Status: " + JSON.stringify(status));
-        console.log("Error Headers: " + JSON.stringify(headers));
-        console.log("Error Config: " + JSON.stringify(config));
+//        console.log("Error Data: " + JSON.stringify(data));
+//        console.log("Error Status: " + JSON.stringify(status));
+//        console.log("Error Headers: " + JSON.stringify(headers));
+//        console.log("Error Config: " + JSON.stringify(config));
 
         // if we don't get query metrics, estimate elapsed time
         if (!data || !data.metrics) {
