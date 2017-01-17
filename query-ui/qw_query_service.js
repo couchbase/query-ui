792cca6c86faa4abc6c2b8556813f5424b9edf1e
Eben Haber <eben@couchbase.com>
2017-01-17 13:25:28 -0800

    MB-22323 - backport fix from Spock
    Change-Id: I344923f60f0b6af0f5e9cc7104956dd2a5371001
    Reviewed-on: http://review.couchbase.org/72139
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Eben Haber <eben@couchbase.com>
    Tested-by: Eben Haber <eben@couchbase.com>

--- query-ui/qw_query_service.js
+++ query-ui/qw_query_service.js
@@ -581,7 +581,7 @@
         //      var post_post_ms = new Date().getTime();
 
         // make sure we show errors if the query did not succeed
-        if (data.status == "success") {
+        if (data && data.status == "success") {
           // if the results are empty, show some of the surrounding data
           if (_.isArray(data.results) && data.results.length == 0) {
             result = {};
@@ -592,7 +592,7 @@
           else
             result = data.results;
         }
-        else {
+        else if (data && data.errors) {
           var failed = "Authorization Failed";
           // hack - detect authorization failed, make a suggestion
           for (var i=0; i < data.errors.length; i++)
@@ -603,6 +603,8 @@
 
           result = data.errors;
         }
+        else
+          result = {error: "Empty result from server, but status: " + status};
 
         // if we got no metrics, create a dummy version
 
@@ -975,8 +977,7 @@
       // if no long ints, just return the original bytes parsed
 
       if (longIntCount == 0) try {
-        var result = JSON.parse(rawBytes);
-        result.rawJSON = rawBytes;
+        return(JSON.parse(rawBytes));
       }
       catch (e) {
         return(rawBytes);
@@ -1006,11 +1007,123 @@
         newBytes += curBytes;
         //console.log("New raw: " + newBytes);
         result = JSON.parse(newBytes);
-        result.rawJSON = rawBytes;
 
+        // see if we can pull just the result out of the rawBytes
+        var rawResult = findResult(rawBytes);
+
+        if (rawResult)
+          result.rawJSON = '\t' + rawResult;
+        else
+          result.rawJSON = rawBytes;
+
+        return result;
       }
-      return result;
     }
+
+    //
+    // getRawResultsFromBytes
+    //
+    // there's a lot of stuff coming back with query results, but we want only the
+    // results themselves. With long numbers, we have to pull those results out of
+    // the raw bytes without parsing them.
+    //
+
+    function findResult(buffer) {
+      // the stuff coming back from the server is a JSON object: "{" followed by
+      // quoted field names, ":", and a JSON value (which is recursive). Since we want
+      // to find the results without parsing, find the "results: " key, then figure
+      // out where it ends.
+
+      var curLoc = 0;
+      var whitespace = /\s/;
+      var len = buffer.length;
+
+      while (curLoc < len && whitespace.test(buffer.charAt(curLoc))) curLoc++; // ignore whitespace
+
+      if (curLoc >= len && buffer.charAt(curLoc) != '{')
+        return null; // expect object start
+      else
+        curLoc++;
+
+      // loop through each field/value until we see a close brace
+
+      while (curLoc < len) {
+        // past the opening of the object, now look for quoted field names followed by ":"
+        while (curLoc < len && whitespace.test(buffer.charAt(curLoc))) curLoc++; // ignore whitespace
+
+        if (curLoc >= len || buffer.charAt(curLoc) != '"') // expect open quote
+          return null; // expect field name start, otherwise we are done
+        else
+          curLoc++;
+
+        var fieldStart = curLoc++;
+        curLoc = moveToEndOfString(buffer,curLoc);
+        if (curLoc >= len) return(null); //make sure we didn't go off the end
+
+        var fieldName = buffer.substring(fieldStart,curLoc);
+        //console.log("Got field: " + fieldName);
+
+        var valueStart = curLoc + 3;
+        curLoc = moveToEndOfValue(buffer,curLoc + 1); // start after close quote
+
+        //console.log("raw: " + buffer.substring(fieldStart-1,curLoc-1));
+
+        if (curLoc < len && fieldName == "results")
+          return(buffer.substring(valueStart,curLoc-1));
+      }
+    }
+
+    //
+    // utility function to traverse strings, finding the end
+    //
+
+    function moveToEndOfString(buffer,curLoc) {
+      while (curLoc < buffer.length) {     // loop until close quote
+        var cur = buffer.charAt(curLoc);
+        if (cur == '\\')
+          curLoc += 2; // skip quoted characters
+        else if (cur != '"')
+          curLoc ++;
+        else
+          break;
+      }
+      return curLoc;
+    }
+
+    // utility function to find the end of a value, which might be an number, string,
+    // object, or array, whose value ends with a comma or a close brace (marking
+    // the end of everything)
+
+    function moveToEndOfValue(buffer,curLoc) {
+      // now parse the value, which might be an number, string, object, or array,
+      // whose value ends with a comma or a close brace (marking the end of everything)
+
+      var braceCount = 0;
+      var bracketCount = 0;
+
+      while (curLoc < buffer.length) {
+        //console.log(curLoc + ": " + buffer.charAt(curLoc) + ", braces: " + braceCount + ", brackets: " + bracketCount);
+        switch (buffer.charAt(curLoc++)){
+        case '{': braceCount++; break;
+        case '}': // if we're not inside an array or object, we're done
+          if (braceCount == 0 && bracketCount == 0)
+            return(curLoc);
+          else
+            braceCount--;
+          break;
+        case '[': bracketCount++; break;
+        case ']': bracketCount--; break;
+        case '"': curLoc = moveToEndOfString(buffer,curLoc) + 1; break;
+        case ',':
+          if (braceCount == 0 && bracketCount == 0)
+            return(curLoc);
+          break;
+        default: // ignore other characters
+        }
+      }
+      return(curLoc);
+    }
+
 
     //
     // load state from storage if possible
