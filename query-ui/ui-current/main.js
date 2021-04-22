5d5a9138f9dd0cdf40c35228a71b67aa2fd0fd7f
Pavel Blagodov <stochmail@gmail.com>
2021-04-22 18:32:14 +0100

    MB-45862: use mnLazyload in order to load modules
    Change-Id: I2fc98e601f3e372839a1948eafe7e3f442e22714
    Reviewed-on: http://review.couchbase.org/c/query-ui/+/151897
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Raluca Lupu <raluca.lupu@couchbase.com>
    Reviewed-by: Eben Haber <eben@couchbase.com>

--- query-ui/ui-current/main.js
+++ query-ui/ui-current/main.js
@@ -1,5 +1,6 @@
 import angular from "/ui/web_modules/angular.js";
 import app from "/ui/app/app.js";
+import { mnLazyload } from "/ui/app/mn.app.imports.js";
 import ace from '/ui/libs/ace/ace-wrapper.js';
 
 import { NgModule } from '/ui/web_modules/@angular/core.js';
@@ -77,19 +78,15 @@ class QueryUI {
           {
             name: "app.admin.docs.**",
             url: "/docs",
-            loadChildren: () => {
-              return import('/_p/ui/query/angular-components/qw.documents.module.js').then(m => {
-                return m.QwDocumentsModule;
-              });
-            }
+            loadChildren: () =>
+              mnLazyload("/_p/ui/query/angular-components/qw.documents.module.js",
+                         "QwDocumentsModule")
           }, {
             name: "app.admin.query.**",
             url: "/query",
-            lazyLoad: ($transition$) => {
-              return import('/_p/ui/query/ui-current/query.js').then(m => {
-                $transition$.injector().get('$ocLazyLoad').load({name: 'qwQuery'});
-              });
-            }
+            lazyLoad: ($transition$) =>
+              mnLazyload("/_p/ui/query/ui-current/query.js",
+                         "qwQuery", $transition$)
           },
           //   {
           //   name: "app.admin.hello_world.**",
