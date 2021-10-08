77f66cfe11dc8fd4b37e9c07f6726f07f58c3dcf
Pavel Blagodov <stochmail@gmail.com>
2021-10-08 11:11:54 +0100

    MB-48738: remove top-level this
    top-level "this" will be replaced with undefined since
    this file is an ECMAScript module
    
    Change-Id: I0d75a2d157055a7dd661a5d7ec8de3cf8e0ccf10
    Reviewed-on: http://review.couchbase.org/c/query-ui/+/163271
    Reviewed-by: Matthew <matthew.dawber@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>

--- query-ui/angular-directives/qw.json.table.editor.directive.js
+++ query-ui/angular-directives/qw.json.table.editor.directive.js
@@ -15,7 +15,7 @@
  *
  */
 
-var my_decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+var my_decorate = function (decorators, target, key, desc) {
   var c = arguments.length,
     r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
   if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
