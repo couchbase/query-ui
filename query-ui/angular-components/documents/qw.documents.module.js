11e7392d4e1d41ee9cf80b2f041bf4e318bf9123
Blair Watt <18262146+udkyo@users.noreply.github.com>
2022-03-08 15:54:54 +0000

    CBD-4161: BSL License updates - Couchbase Server Version 7.1
    Change-Id: Ia50b7b1bd631f1e66b16a9341abfce30f0824b10
    Reviewed-on: https://review.couchbase.org/c/query-ui/+/171919
    Tested-by: Blair Watt <blair.watt@couchbase.com>
    Reviewed-by: Eben Haber <eben@couchbase.com>

--- query-ui/angular-components/documents/qw.documents.module.js
+++ query-ui/angular-components/documents/qw.documents.module.js
@@ -5,7 +5,7 @@ Use of this software is governed by the Business Source License included in
 the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
 file, in accordance with the Business Source License, use of this software will
 be governed by the Apache License, Version 2.0, included in the file
-licenses/APLtxt.
+licenses/APL2.txt.
 */
 
 import { NgModule }               from '@angular/core';
